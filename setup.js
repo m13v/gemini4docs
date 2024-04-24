a8b275d03b650498c9b5ffbd25ac9e24a6ba9b71
matthew-heartful <matthew.heartful@gmail.com>
2024-04-24 13:00:30 -0700

    m

--- setup.js
+++ setup.js
@@ -1,17 +1,19 @@
-import inquirer from 'inquirer';
-console.log('Setting up [Your Package Name]...');
+console.log('Yo, glad you are here! Send me a hi message at i@m13v.com or Discord: matthew.ddy or https://t.me/matthew_ddi')
+console.log('Basic command to get started is g4d')
+// import inquirer from 'inquirer';
+// console.log('Setting up gemini4docs...');
 
-inquirer.prompt([
-    {
-        type: 'confirm',
-        name: 'continue',
-        message: 'Do you want to configure the package now?',
-        default: true
-    }
-]).then(answers => {
-    if (answers.continue) {
-        console.log('Yo, glad you are here! Send me a hi message at i@m13v.com or Discord: matthew.ddy or https://t.me/matthew_ddi');
-        console.log('Basic command to get started is g4d')
-        // Additional setup or configuration tasks here
-    }
-});
+// inquirer.prompt([
+//     {
+//         type: 'confirm',
+//         name: 'continue',
+//         message: 'Do you want to configure the package now?',
+//         default: true
+//     }
+// ]).then(answers => {
+//     if (answers.continue) {
+//         console.log('Yo, glad you are here! Send me a hi message at i@m13v.com or Discord: matthew.ddy or https://t.me/matthew_ddi');
+//         console.log('Basic command to get started is g4d')
+//         // Additional setup or configuration tasks here
+//     }
+// });
