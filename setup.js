4717f71076d1e535f283da73d27785311e167331
matthew-heartful <matthew.heartful@gmail.com>
2024-04-24 12:52:14 -0700

    welcome message

---
+++ setup.js
@@ -0,0 +1,18 @@
+const inquirer = require('inquirer');
+
+console.log('Setting up [Your Package Name]...');
+
+inquirer.prompt([
+    {
+        type: 'confirm',
+        name: 'continue',
+        message: 'Do you want to configure the package now?',
+        default: true
+    }
+]).then(answers => {
+    if (answers.continue) {
+        console.log('Yo, glad you are here! Send me a hi message at i@m13v.com or Discord: matthew.ddy or https://t.me/matthew_ddi');
+        console.log('Basic command to get started is g4d'
+        // Additional setup or configuration tasks here
+    }
+});
