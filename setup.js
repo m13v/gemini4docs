5321b5aab2024596c0f5e4bf9aa205424d332827
matthew-heartful <matthew.heartful@gmail.com>
2024-04-24 13:33:38 -0700

    l

--- setup.js
+++ setup.js
@@ -1,5 +1,26 @@
-console.log('Yo, glad you are here! Send me a hi message at i@m13v.com or Discord: matthew.ddy or https://t.me/matthew_ddi')
-console.log('Basic command to get started is g4d')
+import inquirer from 'inquirer';
+
+const questions = [
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your name?',
+  },
+  {
+    type: 'confirm',
+    name: 'confirm',
+    message: 'Are you sure?',
+  }
+];
+
+if (!process.env.SKIP_PROMPT) {
+  inquirer.prompt(questions).then(answers => {
+    console.log(`Hello, ${answers.username}!`);
+  });
+} else {
+  console.log("Prompt skipped due to SKIP_PROMPT being set.");
+}
+
 // import inquirer from 'inquirer';
 // console.log('Setting up gemini4docs...');
 
